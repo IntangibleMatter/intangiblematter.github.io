adae93bf932ea1ed867983eef390cf80ef734146
IntangibleMatter <47702559+IntangibleMatter@users.noreply.github.com>
2021-04-03 12:29:19 -0700

    Opened repo

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var text;
+
+function parseJson() {
+    var oXHR = new XMLHttpRequest();
+    oXHR.onreadystatechange = reportStatus;
+    oXHR.open("GET", "json/text.json", true); // get json file.
+    oXHR.send();
+
+    function reportStatus() {
+        if (oXHR.readyState == 4) {
+            //console.log(this.responseText)
+            text = JSON.parse(this.responseText);
+        };
+    };
+    //easiest way to fix the string bug
+};
+
+
+function newText() {
+    document.getElementById("text").innerHTML = text.Text[Math.floor(Math.random() * text.Text.length)];
+};
