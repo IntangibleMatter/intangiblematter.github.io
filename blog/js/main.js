b7d22148a287111a1a2f05f4233ab28ba9271203
IntangibleMatter <47702559+IntangibleMatter@users.noreply.github.com>
2021-04-14 18:51:25 -0700

    Started making table

--- blog/js/main.js
+++ blog/js/main.js
@@ -1,3 +1,4 @@
+var postsJSON;
 var posts;
 
 function parseJson() {
@@ -9,19 +10,27 @@ function parseJson() {
     function reportStatus() {
         if (oXHR.readyState == 4) {
             //console.log(this.responseText)
-            text = JSON.parse(this.responseText);
+            postsJSON = JSON.parse(this.responseText);
         };
     };
     //easiest way to fix the string bug
+    posts = postsJSON.Posts;
 };
 
 
 function getNewest() {
-    let newest = posts.Posts.length[posts.Posts.length - 1];
+    let newest = posts.length[posts.length - 1];
     let newThumb = '<img class="newIcon" src=".' + newest.icon + '" />';
     newThumb = '<button class="newPost">';
     newThumb += '<b>' + newest.Title + '</b> <br>';
     newThumb += newest.Description;
     newThumb += "</button>"
     document.getElementById("newest").innerHTML = newThumb;
+};
+
+function loadArchive() {
+    let archiveTable = '<!-- <table> <th><!--icon--></th> --> <th>Post</th><th>Description</th><th>Published</th>';
+    for (post = posts.length - 1; i >= 0; i--) {
+        archiveTable += '<tr> <img class="postIcon" src="' + post.icon + '" /><td><a href="' + post.Link + '">' + post.Title + '</a></td>';
+    };
 };
