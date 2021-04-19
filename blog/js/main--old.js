48b2466248a3ea51eea1ea89dc97b40df77f91b4
IntangibleMatter <47702559+IntangibleMatter@users.noreply.github.com>
2021-04-18 21:51:40 -0700

    ts - got newpost css to work

--- blog/js/main--old.js
+++ blog/js/main--old.js
@@ -1,41 +1,40 @@
-var postsJSON = { "Posts": [] };
+var postsJSON = {};
 var posts = [];
+
 function parseJson() {
     var oXHR = new XMLHttpRequest();
     oXHR.onreadystatechange = reportStatus;
     oXHR.open("GET", "json/posts.json", true); // get json file.
     oXHR.send();
+
     function reportStatus() {
         if (oXHR.readyState == 4) {
             //console.log(this.responseText)
             postsJSON = JSON.parse(this.responseText);
-        }
-        ;
-    }
-    ;
+        };
+    };
     //reportStatus()
     //easiest way to fix the string bug
     posts = postsJSON.Posts;
     console.log(posts);
-}
-;
+};
+
+
 function getNewest() {
-    var newest = posts.length[posts.length - 1];
-    var newThumb = '<img class="newIcon" src=".' + newest.icon + '" />';
+    let newest = posts.length[posts.length - 1];
+    let newThumb = '<img class="newIcon" src=".' + newest.icon + '" />';
     newThumb = '<div class="newPost">';
     newThumb += '<a href="' + newest.Link + '" <h2>' + newest.Title + '</h2></a>';
     newThumb += '<p>' + newest.Description + '</p>';
-    newThumb += "</button>";
+    newThumb += "</button>"
     document.getElementById("newest").innerHTML = newThumb;
     console.log("newest");
-}
-;
+};
+
 function loadArchive() {
-    var archiveTable = '<th>Post</th><th>Description</th><th>Published</th>';
-    for (var post = posts.length - 1; post >= 0; post--) {
+    let archiveTable = '<th>Post</th><th>Description</th><th>Published</th>';
+    for (var post = posts.length - 1; i >= 0; i--) {
         archiveTable += '<tr> <img class="postIcon" src="' + posts[post].icon + '" /><td><a href="' + posts[post].Link + '">' + posts[post].Title + '</a></td>';
-    }
-    ;
+    };
     document.getElementById("posts").innerHTML = archiveTable;
-}
-;
+};
