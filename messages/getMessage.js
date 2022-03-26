47e1be403f41dc35f74182f18c9157d9a612b1c7
IntangibleMatter <47702559+IntangibleMatter@users.noreply.github.com>
2022-03-26 14:37:52 -0700

    names, doctype

--- messages/getMessage.js
+++ messages/getMessage.js
@@ -1,6 +1,27 @@
 var messages;
+var names;
 
 function parseJSON() {
+    parseNames();
+    parseMessages();
+};
+
+function parseNames() {
+    var oXHR = new XMLHttpRequest();
+    oXHR.onreadystatechange = reportStatus;
+    oXHR.open("GET", "names.json", true); // get json file.
+    oXHR.send();
+
+    function reportStatus() {
+        if (oXHR.readyState == 4) {
+            console.log(this.responseText);
+            messages = JSON.parse(this.responseText);
+        };
+    };
+    //easiest way to fix the string bug
+}
+
+function parseMessages() {
     var oXHR = new XMLHttpRequest();
     oXHR.onreadystatechange = reportStatus;
     oXHR.open("GET", "messages.json", true); // get json file.
@@ -8,18 +29,18 @@ function parseJSON() {
 
     function reportStatus() {
         if (oXHR.readyState == 4) {
-            //console.log(this.responseText)
+            console.log(this.responseText);
             messages = JSON.parse(this.responseText);
         };
     };
     //easiest way to fix the string bug
-};
+}
 
 function getmessage() {
     var message;
     var sid = document.getElementById('sid').value.toString();
     console.log(sid);
-
+    console.log(names);
     message = messages[sid];
 
     if (message === undefined) {message = "We're sorry, but your student ID wasn't found.<br>Maybe you made a typo, or maybe we did, or maybe you simply weren't included (Likely the case if you're not a 2024 grad.) Maybe try again? :)";} // Student ID wasn't found in list.
