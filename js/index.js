ebf7bdd73413d5ae0a1c1203eef3572bd15bbcd5
IntangibleMatter <47702559+IntangibleMatter@users.noreply.github.com>
2023-01-16 11:51:06 -0800

    Rework 2023 (#2)
    * A WHOLE NEW LOOK BABYYY
    
    * start on style for new site
    
    * hugoooooooooooooooooooooooooooo

--- js/index.js
+++
@@ -1,9 +0,0 @@
-function viewItem(item) {
-    console.log("ASDAD");
-    var infoOverlay = document.getElementsByClassName('info-overlay');
-    var infoItem = document.getElementById(item);
-    infoOverlay.style.color="red"
-    infoOverlay.style.display="block";
-    infoOverlay.style.position="0vh";
-    infoOverlay.style.transition="2s"
-}
