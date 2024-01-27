61c377fcdbd40db49cafc705f1eedff4537f3ea0
IntangibleMatter <47702559+IntangibleMatter@users.noreply.github.com>
2024-01-27 12:08:06 -0800

    stars toggle

--- themes/intangibletheme23/static/js/togglelight.js
+++ themes/intangibletheme23/static/js/togglelight.js
@@ -13,35 +13,78 @@ function applyLightMode(element, isLightModeOn) {
 
 function toggleLightMode() {
     const contentDiv = document.querySelector('#main-content');
-    const toggleButton = document.querySelector('#toggleButton');
+    const lighToggleButton = document.querySelector('#lightToggleButton');
     const isLightModeOn = !contentDiv.classList.contains('light_mode');
 
     applyLightMode(contentDiv, isLightModeOn);
 
     if (isLightModeOn) {
-        toggleButton.textContent = 'Toggle Dark Mode';
+        lightToggleButton.textContent = 'Toggle Dark Mode';
         localStorage.setItem('lightModePreference', 'on');
     } else {
-        toggleButton.textContent = 'Toggle Light Mode';
+        lightToggleButton.textContent = 'Toggle Light Mode';
         localStorage.setItem('lightModePreference', 'off');
     }
 }
 
-const toggleButton = document.querySelector('#toggleButton');
-toggleButton.addEventListener('click', toggleLightMode);
+const lightToggleButton = document.querySelector('#lightToggleButton');
+lightToggleButton.addEventListener('click', toggleLightMode);
+
+const starsToggleButton = document.querySelector('#starsToggleButton');
+starsToggleButton.addEventListener('click', toggleStarsMode);
 
 // Retrieve light mode preference from localStorage on page load
 document.addEventListener('DOMContentLoaded', function () {
     const contentDiv = document.querySelector('#main-content');
-    const toggleButton = document.querySelector('#toggleButton');
+    const lightToggleButton = document.querySelector('#lightToggleButton');
 
     const lightModePreference = localStorage.getItem('lightModePreference');
 
     if (lightModePreference === 'on') {
         applyLightMode(contentDiv, true);
-        toggleButton.textContent = 'Toggle Dark Mode';
+        lightToggleButton.textContent = 'Toggle Dark Mode';
     } else {
         applyLightMode(contentDiv, false);
-        toggleButton.textContent = 'Toggle Light Mode';
+        lightToggleButton.textContent = 'Toggle Light Mode';
     }
+    const starsToggleButton = document.querySelector('#starsToggleButton');
+	starsToggleButton.addEventListener('click', toggleStarsMode);
+    const starsModePreference = localStorage.getItem('starsModePreference');
+
+    if (starsModePreference === 'on') {
+        applyStarsMode(true);
+        starsToggleButton.textContent = 'Star Motion Off';
+    } else {
+        applyStarsMode(false);
+        starsToggleButton.textContent = 'Star Motion On';
+    }
+
 });
+
+function applyStarsMode(isStarsModeOn) {
+	const children = document.querySelector("#stars-container").children;
+    for (let i = 0; i < children.length; i++) {
+		if (isStarsModeOn) {
+			children[i].classList.add('stars-off');
+		} else {
+			children[i].classList.remove('stars-off');
+		}
+	}
+}
+
+
+function toggleStarsMode() {
+    const contentDiv = document.querySelector('#stars-container');
+    const toggleStars = document.querySelector('#starsToggleButton');
+    const isStarsModeOn = !contentDiv.children[2].classList.contains('stars-off');
+
+    applyStarsMode(isStarsModeOn)
+
+    if (isStarsModeOn) {
+        toggleStars.textContent = 'Star Motion On';
+        localStorage.setItem('starsModePreference', 'on');
+    } else {
+        toggleStars.textContent = 'Star Motion Off';
+        localStorage.setItem('starsModePreference', 'off');
+    }
+}
