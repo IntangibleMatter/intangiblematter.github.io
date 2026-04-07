6ab3499101a1870946307e77ecb6e98c6d11b4b9
IntangibleMatter <IntangibleMatter@users.noreply.github.com>
2026-04-07 00:57:30 +0000

    deploy: 9240b3756ac0ceea6a0ad9487679e74a35fc362c

--- js/bskycomments.js
+++ js/bskycomments.js
@@ -63,7 +63,7 @@ async function loadBlueskyComments() {
 			limit: 100,
 		});
 		const searchResponse = await fetch(
-			`https://public.api.bsky.app/xrpc/app.bsky.feed.getAuthorFeeds?${searchParams}`,
+			`https://public.api.bsky.app/xrpc/app.bsky.feed.getAuthorFeed?${searchParams}`,
 		);
 
 		if (!searchResponse.ok) throw new Error("Failed to search posts");
